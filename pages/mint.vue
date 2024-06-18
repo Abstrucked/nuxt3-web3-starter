824a73796cb0512276e848de0e8f047033d7ea6a
abstrucked <mxhgraphics@gmail.com>
2024-06-18 19:21:30 +0200

    added theme-switch and mint form state basic impl

--- pages/mint.vue
+++ pages/mint.vue
@@ -1,11 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const state = reactive({
+  mintAmount: 1,
+  isMinting: false
 
+});
+const mint = () => {
+  state.isMinting = true;
+  setTimeout(() => {
+    state.isMinting = false;
+  }, 2000);
+}
+</script>
 <template>
   <UContainer>
     <h1>Mint</h1>
-    <UForm>
-      <UInput v-model="mintAmount" label="Amount" />
-      <UButton @click="mint" :loading="isMinting">Mint</UButton>
+    <UForm :state="state" >
+      <UInput v-model="state.mintAmount" label="Amount" :disabled="state.isMinting" />
+      <UButton @click="mint" :loading="state.isMinting">Mint</UButton>
     </UForm>
   </UContainer>
 </template>
